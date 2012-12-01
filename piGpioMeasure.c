b3f517a7fba8fd1fe2d8d419803d89e96867844f
Andreas Agvard <andreas.agvard@gmail.com>
2012-12-01 15:30:18 +0100

    Rewrote to use time between signals instead of averaging the number of signal over a period of time.

--- piGpioMeasure.c
+++ piGpioMeasure.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <sys/time.h>
 
 // Configuration
-const int CONF_MEASUREMENT_TIME = 10000;
-const int CONF_AVERAGE_COUNT = 6;
+// -------------
+// Input (according to wiringPi) to use as input
 const int CONF_COUNTER_WIRING_INPUT = 0;
+// Delay after high signal to filter out flutter
+const int CONF_FILTER_DELAY_MS = 25;
+// Delay between polling, lower number increases accuracy and CPU usage
+const int CONF_POLL_DELAY_MS = 2;
 
 // Globals
-const int G_COUNTER_LOCK = 0;
-int gCounter = 0;
+#define G_INVALID_TIME (-1)
+long long g_timeOfPrevSignalMs = G_INVALID_TIME;
+
+void onSignal() {
+    // Get timestamp
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    const long long timeOfSignalMs = (tv.tv_sec * 1000LL) + (tv.tv_usec / 1000LL);
+
+    if (G_INVALID_TIME != g_timeOfPrevSignalMs) {
+        long long dt = timeOfSignalMs - g_timeOfPrevSignalMs;
+        double f_h = 3600000.0 / dt;
+        printf("Time between signals: %lld, Signals per hour: %f\n", dt, f_h);
+    }
+
+    g_timeOfPrevSignalMs = timeOfSignalMs;
+}
 
 PI_THREAD (pollingThread) {
     int prevSig = -1;
     while (1) {
         int sig = digitalRead(CONF_COUNTER_WIRING_INPUT);
         if (sig != prevSig) {
-            // Up event, increment counter
+            // Up event
             if (sig == 1) {
-                piLock(G_COUNTER_LOCK);
-                gCounter++;
-                piUnlock(G_COUNTER_LOCK);
+                onSignal();
             }
             // Down event, delay to filter out flutter
             else if (sig == 0) {
-                delay(50);
+                delay(CONF_FILTER_DELAY_MS);
             }
 
             prevSig = sig;
         }
 
         // Delay in order to reduce CPU usage
-        delay(2);
+        delay(CONF_POLL_DELAY_MS);
     }
 }
 
@@ -50,16 +68,20 @@ int main() {
 
     printf("Started\n");
 
+    // TODO Figure out what to do in this thread
+    while (1);
+
+    /* TODO Remove
+
+    // Initialize array for average calculations
     int arrayPos = 0;
     int counterArray[CONF_AVERAGE_COUNT];
-
     for (i = 0; i < CONF_AVERAGE_COUNT; ++i) {
         counterArray[i] = 0;
     }
 
     // Get counter at regular intervals
     while (1) {
-        // TODO
         delay(CONF_MEASUREMENT_TIME);
 
         // Get and reset counter
@@ -88,4 +110,5 @@ int main() {
             arrayPos = 0;
         }
     }
+    */
 }
